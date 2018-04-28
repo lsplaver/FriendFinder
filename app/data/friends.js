51726e4d996491c8ce250d283a50be53f192f735
unknown <lsplaver@yahoo.com>
2018-04-28 09:35:22 -0400

    questions working, backend weird asynch errors in apiRoutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,32 @@ var friends = [
         name: "Jeff",
         photo: "placeholder",
         scores: [
-            0,
-            1,
-            2,
             3,
+            10,
             4,
-            5,
-            6,
-            7,
             8,
-            9
+            6,
+            5,
+            1,
+            7,
+            4,
+            1
         ]
     },
     {
         name: "Dan",
-        phote: "placeholder2",
+        photo: "placeholder2",
         scores: [
+            1,
+            6,
+            7,
+            8,
+            3,
+            8,
             10,
-            20,
-            30,
-            40,
-            50,
-            60,
-            70,
-            80,
-            90,
-            100
+            1,
+            9,
+            6
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,8 +2,193 @@ var express = require("express");
 var path = require("path");
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
-        return res.json(friends);
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        var newUser = req.body;
+        console.log(newUser);
+        // var results = "test";
+
+        var totalDifferences = {
+            name: "",
+            totalDifference: ""
+        };
+        for (var i = 0; i < newUser.scores.length; i++) {
+            newUser.scores[i] = Number(newUser.scores[i]);
+        };
+
+        console.log("newUser after converting scores to numbers \n");
+        console.log(newUser);
+
+        var tempResults = [friends.length];
+        tempResults
+
+        // var populateTempResults = function(){}, determineResults = function(){}, populateTempResults2 = function(){};
+
+        var x = 0, y = 0, z = 0, individualDifferences = 0;
+
+
+        var populateTempResults3 = function () {
+            console.log(totalDifferences);
+            console.log("tempResults at start of populateTempResults3");
+            console.log(tempResults);
+
+            console.log("friends[" + x + "].name is: " + friends[x].name);
+            console.log("tempResults before setting tempDifferences.name = friends[x].name");
+            console.log(tempResults);
+            totalDifferences.name = friends[x].name;
+            console.log("tempResults after setting tempDifferences.name = friendsa[x].name");
+            console.log(tempResults);
+            console.log("totalDifferences is: ");
+            console.log(totalDifferences);
+            console.log("tempResults before setting tempDifferences.difference = individualDifferences");
+            console.log(tempResults);
+            totalDifferences.totalDifference = individualDifferences;
+            console.log("tempResults after setting tempDifferences.difference = individualDifferences");
+            console.log(tempResults);
+            console.log("the updates totalDifferences.totalDifference is: " + totalDifferences.totalDifference);
+            console.log("value of tempResults before push");
+            console.log(tempResults);
+            console.log("the current length of tempResults before the push: " + tempResults.length);
+            tempResults.push(totalDifferences);
+            console.log("tempResults after pushing totalDifferences");
+            console.log(tempResults);
+            console.log("before incrementing x: " + x);
+            x++;
+            console.log("after incrementing x: " + x);
+            populateTempResults();
+        }
+
+        var populateTempResults2 = function () {
+            if (y < 10) {
+                console.log("position y is: " + y);
+                var tempNum = 0;
+                console.log("tempNum is: " + tempNum);
+
+                console.log("friends[" + x + "].scores[" + y + "] is: " + friends[x].scores[y]);
+                tempNum = Math.abs(friends[x].scores[y] - newUser.scores[y]);
+                console.log("the value of tempNum after absolute value is: " + tempNum);
+                individualDifferences += tempNum;
+                console.log("the updated value of individualDifferences is: " + individualDifferences);
+                y++;
+            }
+
+            else {
+                populateTempResults3();
+            }
+            populateTempResults2();
+        };
+
+        // var totalDifferences = {
+        //     name: "",
+        //     totalDifference: ""
+        // };
+
+        var populateTempResults = function () {
+            // console.log("the value of friends.length is: " + friends.length);
+            // for (var x = 0; x < friends.length; x++) {
+            console.log("current value of x at start of populateTempResults() is: " + x);
+            if (x < friends.length) {
+                console.log("position x is: " + x);
+                var individualDifferences = 0;
+                console.log("individualDifferences is: " + individualDifferences);
+                // for (var y = 0; y < 10; y++) {
+                populateTempResults2(y = 0);
+                // var populateTempResults2 = function () {
+                //     if (y < 10) {
+                //         console.log("position y is: " + y);
+                //         var tempNum = 0;
+                //         console.log("tempNum is: " + tempNum);
+
+                //         console.log("friends[" + x + "].scores[" + y + "] is: " + friends[x].scores[y]);
+                //         tempNum = Math.abs(friends[x].scores[y] - newUser.scores[y]);
+                //         console.log("the value of tempNum after absolute value is: " + tempNum);
+                //         individualDifferences += tempNum;
+                //         console.log("the updated value of individualDifferences is: " + individualDifferences);
+                //         y++;
+                //     };
+                //     populateTempResults2();
+                // };
+
+                // console.log(totalDifferences);
+
+                // console.log("friends[" + x + "].name is: " + friends[x].name);
+                // totalDifferences.name = friends[x].name;
+                // console.log("totalDifferences is: ");
+                // console.log(totalDifferences);
+                // totalDifferences.totalDifference = individualDifferences;
+                // console.log("the updates totalDifferences.totalDifference is: " + totalDifferences.totalDifference);
+                // tempResults.push(totalDifferences);
+                // console.log("tempResults after pushing totalDifferences");
+                // console.log(tempResults);
+                // x++;
+            };
+            // populateTempResults();
+            determineResults();
+        };
+
+        var passResult = function () {
+            console.log("final value of tempResults is:");
+            console.log(tempResults);
+
+            var results = tempResults;
+
+            console.log("value of results is: ");
+            console.log(results);
+
+            res.json(results);
+        };
+
+        var determineResults = function () {
+            // for (var x = 0; x < tempResults.length; x++) {
+            console.log("the value of tempRsults.length at beinging of determineResults() is: " + tempResults.length);
+            console.log("the current value of tempResults is: ");
+            console.log(tempResults);
+            if (z < tempResults.length) {
+                console.log("z is: " + z);
+                console.log("current length of tempResults before inner if: " + tempResults.length);
+                if (tempResults[z].totalDifference < tempResults[z + 1].totalDifference) {
+                    tempResults.slice(z, z);
+                    console.log("tempResults.length after slice: " + tempResults.length);
+                    console.log("new value of tempResults is:");
+                    console.log(tempResults);
+                    console.log("new value of x is: " + z);
+                }
+
+                else if (tempResults[z].totalDifference > tempResults[z + 1].totalDifference) {
+                    tempResults.slice((z + 1), (z + 1));
+                    console.log("tempResults.length after slice: " + tempResults.length);
+                    console.log("new value of tempResults is:");
+                    console.log(tempResults);
+                    console.log("new value of x is: " + z);
+                }
+
+                else {
+                    z++;
+                }
+                determineResults();
+            };
+            passResult();
+        };
+
+        populateTempResults();
+        // determineResults();
+
+        // var passResult = function() {
+        // console.log("final value of tempResults is:");
+        // console.log(tempResults);
+
+        // var results = tempResults;
+
+        // console.log("value of results is: ");
+        // console.log(results);
+
+        // res.json(results);
+        // };
+        // tempResults = [];
+        // };
     });
 };

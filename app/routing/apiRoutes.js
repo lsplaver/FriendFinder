3c3907474dd6d9f4d5688644cdfba91b51147177
unknown <lsplaver@yahoo.com>
2018-04-28 21:37:10 -0400

    cleaned by removing comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,6 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         var newUser = req.body;
         console.log(newUser);
-        // var results = "test";
 
         var totalDifferences = {
             name: "",
@@ -23,53 +22,37 @@ module.exports = function (app) {
         console.log("newUser after converting scores to numbers \n");
         console.log(newUser);
 
-        // var tempResults = [totalDifferences]; //[friends.length];
         var tempName = [];
         var aLength = -9999;
         aLength = friends.length;
-        tempName.length = aLength; // friends.length; //.valueOf(0); //: friends.length;
+        tempName.length = aLength;
         var tempDiff = [];
-        tempDiff.length = aLength; // friends.length;
+        tempDiff.length = aLength;
         var tempResults = {tempName, tempDiff};
-        // tempResults
-
-        // var populateTempResults = function(){}, determineResults = function(){}, populateTempResults2 = function(){};
 
         var x = 0, y = 0, z = 0, individualDifferences = 0;
 
 
         var populateTempResults3 = function () {
             if (resultsPassed === false) {
-                // if (x >= tempResults.tempDiff.length) {
-                //     // var tempInflateResults = {};
-                //     // tempInflateResults = tempResults[x - 1];
-                //     // tempResults.push(tempInflateResults);
-                //     tempResults.push(totalDifferences);
-                // }
-                // console.log(totalDifferences);
                 console.log("tempResults at start of populateTempResults3");
                 console.log(tempResults);
 
                 console.log("friends[" + x + "].name is: " + friends[x].name);
                 console.log("tempResults before setting tempDifferences.name = friends[x].name");
                 console.log(tempResults);
-                // totalDifferences.name = friends[x].name;
                 tempResults.tempName[x] = friends[x].name;
                 console.log("tempResults after setting tempDifferences.name = friendsa[x].name");
                 console.log(tempResults);
                 console.log("totalDifferences is: ");
-                // console.log(totalDifferences);
                 console.log("tempResults before setting tempDifferences.difference = individualDifferences");
                 console.log(tempResults);
-                // totalDifferences.totalDifference = individualDifferences;
                 tempResults.tempDiff[x] = individualDifferences;
                 console.log("tempResults after setting tempDifferences.difference = individualDifferences");
                 console.log(tempResults);
-                // console.log("the updates totalDifferences.totalDifference is: " + totalDifferences.totalDifference);
                 console.log("value of tempResults before push");
                 console.log(tempResults);
                 console.log("the current length of tempResults before the push: " + tempDiff.length);
-                // tempResults.push(totalDifferences);
                 console.log("tempResults after pushing totalDifferences");
                 console.log(tempResults);
                 console.log("before incrementing x: " + x);
@@ -108,52 +91,15 @@ module.exports = function (app) {
             }
         };
 
-        // var totalDifferences = {
-        //     name: "",
-        //     totalDifference: ""
-        // };
-
         var populateTempResults = function () {
             if (resultsPassed === false) {
-                // console.log("the value of friends.length is: " + friends.length);
-                // for (var x = 0; x < friends.length; x++) {
                 console.log("current value of x at start of populateTempResults() is: " + x);
                 if (x < friends.length) {
                     console.log("position x is: " + x);
                     var individualDifferences = 0;
                     console.log("individualDifferences is: " + individualDifferences);
-                    // for (var y = 0; y < 10; y++) {
                     populateTempResults2(y = 0);
-                    // var populateTempResults2 = function () {
-                    //     if (y < 10) {
-                    //         console.log("position y is: " + y);
-                    //         var tempNum = 0;
-                    //         console.log("tempNum is: " + tempNum);
-
-                    //         console.log("friends[" + x + "].scores[" + y + "] is: " + friends[x].scores[y]);
-                    //         tempNum = Math.abs(friends[x].scores[y] - newUser.scores[y]);
-                    //         console.log("the value of tempNum after absolute value is: " + tempNum);
-                    //         individualDifferences += tempNum;
-                    //         console.log("the updated value of individualDifferences is: " + individualDifferences);
-                    //         y++;
-                    //     };
-                    //     populateTempResults2();
-                    // };
-
-                    // console.log(totalDifferences);
-
-                    // console.log("friends[" + x + "].name is: " + friends[x].name);
-                    // totalDifferences.name = friends[x].name;
-                    // console.log("totalDifferences is: ");
-                    // console.log(totalDifferences);
-                    // totalDifferences.totalDifference = individualDifferences;
-                    // console.log("the updates totalDifferences.totalDifference is: " + totalDifferences.totalDifference);
-                    // tempResults.push(totalDifferences);
-                    // console.log("tempResults after pushing totalDifferences");
-                    // console.log(tempResults);
-                    // x++;
                 };
-                // populateTempResults();
                 determineResults();
             }
 
@@ -169,7 +115,7 @@ module.exports = function (app) {
                 console.log("final value of tempResults is:");
                 console.log(tempResults);
 
-                var results = []; // = tempResults;
+                var results = [];
                 results.length = 2;
                 var aName = tempResults.tempName[0];
                 friends.findIndex
@@ -184,7 +130,6 @@ module.exports = function (app) {
                 console.log(results);
 
                 resultsPassed = passed;
-                // return res.json(results);
             }
             else {
                 console.log("results already passed");
@@ -195,7 +140,6 @@ module.exports = function (app) {
 
         var determineResults = function () {
             if (resultsPassed === false) {
-                // for (var x = 0; x < tempResults.tempDiff.length; x++) {
                 console.log("the value of tempRsults.length at beinging of determineResults() is: " + tempDiff.length);
                 console.log("the current value of tempResults is: ");
                 console.log(tempResults);
@@ -238,20 +182,5 @@ module.exports = function (app) {
         };
 
         populateTempResults();
-        // determineResults();
-
-        // var passResult = function() {
-        // console.log("final value of tempResults is:");
-        // console.log(tempResults);
-
-        // var results = tempResults;
-
-        // console.log("value of results is: ");
-        // console.log(results);
-
-        // res.json(results);
-        // };
-        // tempResults = [];
-        // };
     });
 };
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-// var apiRoutes = require('./app/routing/apiRoutes');
-// var htmlRoutes = require('./app/routing/htmlRoutes');
 
 var app = express();
 
@@ -11,8 +9,6 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(apiRoutes());
-// app.use(htmlRoutes());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
